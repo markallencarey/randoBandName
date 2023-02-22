c43c18d90e065a9486f8ca2c6155af0ffef08e6d
markallencarey <markallencarey@gmail.com>
2023-02-21 23:14:11 -0700

    yarnpod

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -7,7 +7,7 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 //Navigation
 //Components
 //Functions
-import { getBandName } from '../../Functions/getBandName'
+import { getWord } from '../../Functions/getWord'
 //Screens
 //Icons
 //Images
@@ -17,51 +17,30 @@ import { Buttons, Colors, Containers, Fonts, Icons, Images, Index, Misc, Window 
 
 export const Home = ({ navigation }) => {
 	const [adjInfo, setAdjInfo] = useState({})
-	// console.log('file: Home.js -> line 20 -> Home -> adjInfo', adjInfo)
 	const [nounInfo, setNounInfo] = useState({})
-	// console.log('file: Home.js -> line 22 -> Home -> nounInfo', nounInfo)
 	const [bandName, setBandName] = useState('')
-	// console.log('file: Home.js -> line 24 -> Home -> bandName', bandName)
 
-	// const pressGetBandName = () => {
-	// 	// if (adjInfo.type !== 'adjective') {
-	// 	getBandName().then(adj => {
-	// 		console.log('file: Home.js -> line 29 -> getBandName -> word', adj.word)
-	// 		// setAdjInfo(adj).then(() => {
-	// 		// 	if (nounInfo.type !== 'noun') {
-	// 		// 		getBandName().then(noun => {
-	// 		// 			// console.log('file: Home.js -> line 36 -> getBandName -> word', word.word)
-	// 		// 			setNounInfo(noun).then(() => {
-	// 		// 				setBandName(`The ${adjInfo.word} ${nounInfo.word}s`)
-	// 		// 			})
-	// 		// 		})
-	// 		// 	}
-	// 		// })
-	// 	})
-	// 	// }
-	// }
+	const pressGetWord = () => {
+		let count = 0
 
-	const pressGetBandName = () => {
-		console.log('')
-		console.log('START pressGetBandName()')
+		// if (adjInfo.type !== 'adjective') {
+		getWord().then(adjRes => {
+			console.log('start getWord')
+			setAdjInfo(adjRes)
+			count++
+			console.log('count', count)
+		})
+		// } else {
+		// 	console.log('adj type is already', adjInfo.type)
+		// }
 
-		console.log('line 49 -> adjInfo.type', adjInfo.type)
-		if (adjInfo.type !== 'adjective') {
-			getBandName().then(adjRes => {
-				console.log('file: Home.js -> line 51 -> getBandName -> adjRes', adjRes)
-				setAdjInfo(adjRes)
-
-				// if (adjRes.type === 'adjective') {
-				// 	console.log('adjective')
-				// } else {
-				// 	console.log('not adjective')
-				// }
-
-				console.log('')
-				console.log('END pressGetBandName()')
-				console.log('')
-			})
-		}
+		// if (nounInfo.type !== 'noun') {
+		// 	getWord().then(nounRes => {
+		// 		setNounInfo(nounRes)
+		// 	})
+		// } else {
+		// 	console.log('noun type is already', nounInfo.type)
+		// }
 	}
 
 	const resetBandName = () => {
@@ -77,7 +56,7 @@ export const Home = ({ navigation }) => {
 		<View style={styles.content}>
 			<View style={styles.titleView}>
 				<Text style={styles.h2}>Random Band Name Generator</Text>
-				<TouchableOpacity style={styles.navBtn} onPress={pressGetBandName}>
+				<TouchableOpacity style={styles.navBtn} onPress={pressGetWord}>
 					<Text style={styles.body}>Get Band Name</Text>
 				</TouchableOpacity>
 				<View style={styles.adjInfo}>
